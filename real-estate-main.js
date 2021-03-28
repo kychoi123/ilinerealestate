e0bf8e66bdb1c21696d25b7acaacd9684a519e6e
kychoi <78518600+kychoi123@users.noreply.github.com>
2021-03-27 22:19:44 -0400

    Add files via upload

---
+++ real-estate-main.js
@@ -0,0 +1,16 @@
+
+
+    function openFunction(){
+
+        document.getElementById("menu-side").style.width="120px";
+        document.getElementById("mainbox").style.marginLeft="0px";
+       
+    };
+            function closeFunction(){
+
+        document.getElementById("menu-side").style.width="0px";
+        document.getElementById("mainbox").style.marginLeft="0px";
+        document.getElementById("mainbox").innerHTML="&#9776; Open";
+        };
+        
+     
